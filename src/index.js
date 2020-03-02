54a6a1fd3cd2558dea9399d2aa47a58ded8f1370
LyssenkoAlex <lyssenko.alex@gmail.com>
2020-03-02 15:02:06 +0600

    fix: solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return array && array.length > 0  ? array.reduce((block, index) => block < index  ? block : index) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+ return array && array.length > 0 ?   array.reduce((block, index) => block > index  ? block : index) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return array && array.length > 0 ? (array.reduce((block, index) => block + index)) / array.length : 0;
 }
